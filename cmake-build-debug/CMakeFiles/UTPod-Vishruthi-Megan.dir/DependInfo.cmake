f8b79ae9f3e00e6cb67f7b2462ae0e19a70b329b
Megan Nguyen <megantnguyen@utexas.edu>
2018-11-01 02:49:57 -0500

    Wrote comments, created clearMemory, moved numSongs to private

---
+++ cmake-build-debug/CMakeFiles/UTPod-Vishruthi-Megan.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/EE312/CLionProjects/UTPod-Vishruthi-Megan/Song.cpp" "C:/EE312/CLionProjects/UTPod-Vishruthi-Megan/cmake-build-debug/CMakeFiles/UTPod-Vishruthi-Megan.dir/Song.cpp.obj"
+  "C:/EE312/CLionProjects/UTPod-Vishruthi-Megan/UTPOD.cpp" "C:/EE312/CLionProjects/UTPod-Vishruthi-Megan/cmake-build-debug/CMakeFiles/UTPod-Vishruthi-Megan.dir/UTPOD.cpp.obj"
+  "C:/EE312/CLionProjects/UTPod-Vishruthi-Megan/driver.cpp" "C:/EE312/CLionProjects/UTPod-Vishruthi-Megan/cmake-build-debug/CMakeFiles/UTPod-Vishruthi-Megan.dir/driver.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/UTPod-Vishruthi-Megan.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UTPod-Vishruthi-Megan.dir/driver.cpp.obj"
+  "CMakeFiles/UTPod-Vishruthi-Megan.dir/Song.cpp.obj"
+  "CMakeFiles/UTPod-Vishruthi-Megan.dir/UTPOD.cpp.obj"
+  "UTPod-Vishruthi-Megan.pdb"
+  "UTPod-Vishruthi-Megan.exe"
+  "UTPod-Vishruthi-Megan.exe.manifest"
+  "libUTPod-Vishruthi-Megan.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UTPod-Vishruthi-Megan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
